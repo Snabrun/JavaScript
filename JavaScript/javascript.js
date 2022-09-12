ec5dc37e322b477e402de8abfeaccc78b8191082
Egor Shkaroburov <egor.shkaroburov@edu.keuda.fi>
2022-09-12 11:28:23 +0300

    JavaScript

---
+++ JavaScript/javascript.js
@@ -0,0 +1,32 @@
+function laskut()
+{
+var luku1, luku2, summa, erotus, osamaara, tulo;
+luku1 = 10;
+luku2 = 2;
+summa = luku1 + luku2;
+erotus = luku1 - luku2;
+osamaara = luku1 / luku2;
+tulo = luku1 * luku2;
+console.log("Summa = " + summa)
+console.log("Erotus = " + erotus)
+console.log("Osamäärä = " + osamaara)
+console.log("Tulo = " + tulo)
+}
+function buyBitcoin()
+{
+  var x1, x2, x3;
+  x1 = "Buy ";
+  x2 = "the ";
+  x3 = "dip!";
+  console.log(x1 + x2 + x3);
+}
+function attribuutti(nimi)
+{
+  var x1 = " on ";
+  var x2 = "hyvä ihminen";
+  console.log(nimi + x1 + x2);
+}
+function laskeYhteen(luku1, luku2)
+{
+  console.log(luku1 + luku2);
+}
