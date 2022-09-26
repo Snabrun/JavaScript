9b3cfc35534785d48d8dc376433240bba165e859
Egor Shkaroburov <egor.shkaroburov@edu.keuda.fi>
2022-09-26 11:29:50 +0300

    Viikon2 harjoitus. JS

---
+++ JavaScript/Perustehtäviä/Viikon2.teht/teht.1/harjoitus.js
@@ -0,0 +1,26 @@
+function tarkasta()
+{
+  document.getElementById('eka').style.backgroundColor = "blue";
+  document.getElementById('v1').style.border = "2px solid gray";
+var massive = [];
+  console.log(massive);
+  massive[0] = document.getElementById('luku1').value;
+  massive[1] = document.getElementById('luku2').value;
+  massive[2] = document.getElementById('luku3').value;
+  console.log(massive);
+  document.getElementById('v1').innerHTML = "Luvut ovat: " + massive + "<br />Luvut järjestyksessä: " + massive.sort();
+}
+
+function max()
+{
+  document.getElementById('toka').style.backgroundColor = "blue";
+  document.getElementById('v2').style.border = "2px solid gray";
+  var massive1 = [];
+  massive1[0] = document.getElementById('luk1').value;
+  massive1[1] = document.getElementById('luk2').value;
+  massive1[2] = document.getElementById('luk3').value;
+  massive1[3] = document.getElementById('luk4').value;
+  massive1[4] = document.getElementById('luk5').value;
+  console.log(Math.max.apply(Math,massive1));
+  document.getElementById('v2').innerHTML = "Luvut ovat: " + massive1 + "<br>Suurin luku: " + Math.max.apply(null,massive1);
+}
