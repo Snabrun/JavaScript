328321af0537257764c609cb00196d9fe23ed405
Egor Shkaroburov <egor.shkaroburov@edu.keuda.fi>
2022-10-06 11:27:26 +0300

    Viikon2, 2 osa melkein valmis - teht.5 tekematta

--- JavaScript/Perustehtäviä/Viikon2.teht/teht.1 toinen osa/harjoitus.js
+++ JavaScript/Perustehtäviä/Viikon2.teht/teht.1 toinen osa/harjoitus.js
@@ -13,7 +13,8 @@ else {
 function check()
 {
   var paiva = document.getElementById('number').value;
-switch (paiva) {
+switch (paiva)
+{
   case "1":
   document.getElementById("v2").innerHTML = "Maanantai"
     break;
@@ -43,20 +44,33 @@ switch (paiva) {
  }
 }
 
-function karkavuosi()
+function karka()
 {
   var vuosi = document.getElementById('vuodenN').value;
-  if (vuosi/4 && vuosi/100 == 1)
+  if (vuosi % 4 != 0 || (vuosi % 100 == 0 && vuosi % 400 != 0))
   {
-    document.getElementById("v3").innerHTML = "Vuosi on karkavuosi";
-    console.log('vuosi');
-    else if (vuosi/400)
-    {
-      document.getElementById("v3").innerHTML = "Vuosi on karkavuosi";
-    }
-    else
-    {
-      document.getElementById("v3").innerHTML = "Vuosi ei ole karkausvuosi";
-    }
+    document.getElementById("v3").innerHTML = "Tavallinen";
+  }
+  else {
+    document.getElementById("v3").innerHTML = "Karkavuosi";
+  }
+}
+
+function keskiA()
+{
+  var numero1 = document.getElementById('luku4').value;
+  var numero2 = document.getElementById('luku5').value;
+  var numero3 = document.getElementById('luku6').value;
+  var numero4 = document.getElementById('luku7').value;
+  var numero5 = document.getElementById('luku8').value;
+  var summa = Number(numero1) + Number(numero2) + Number(numero3) + Number(numero4) + Number(numero5);
+  var kesk = summa / 5;
+  if (summa > 0 && kesk > 0)
+  {
+    document.getElementById("v4").innerHTML = "Lukujen summa on: " + summa + " ja keskiarvo on: " + kesk;
+  }
+  else
+  {
+    document.getElementById("v4").innerHTML = "Anna joku luku!";
   }
 }
