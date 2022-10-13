8f6efe870b3444010972ba06afbb40547bb5049a
Egor Shkaroburov <egor.shkaroburov@edu.keuda.fi>
2022-10-13 11:34:11 +0300

    ---

---
+++ JavaScript/Projektit/Projekti2/projekti.js
@@ -0,0 +1,12 @@
+var arvatta = Math.floor(Math.random()*10)+1;
+var kerrat = 0;
+var voito = 0;
+function arvaa(arpa)
+{
+  kerrat++;
+  var arvaus = arpa.value;
+  if (kerrat <=3 && )
+  {
+            
+  }
+}
