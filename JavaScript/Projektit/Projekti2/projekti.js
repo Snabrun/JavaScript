5e20b55c6326b6ddfb40c06558e27e319d9ecd78
Egor Shkaroburov <egor.shkaroburov@edu.keuda.fi>
2022-10-24 11:30:19 +0300

    projekti2

--- JavaScript/Projektit/Projekti2/projekti.js
+++ JavaScript/Projektit/Projekti2/projekti.js
@@ -24,5 +24,23 @@ function arvaa(arpa)
       document.getElementById('' +arvaus+'').classList.add("vaarin");
       document.getElementById(''+arvaus+'').disabled = true;      
     }
+    else if (arvus == arvoLuku)
+    {
+      document.getElementById('viesti').innerHTML = 'Oikein, käytit ' + kerrat + ' kertaa<br><button class="taas" onclick="pelaaTaas()">Pelaa taas</button>';
+      document.getElementById(''+arvoLuku+'').classList.toggle("oikein");
+      win = 1;
+      winNumero += 1;
+      document.getElementById('winNumero').innerHTML = winNumero;
+
+    }
+  }
+  else if (kerrat == 3 && win == 0)
+  {
+    if (arvaus < arvoLuku)
+    {
+      document.getElementById('viesti').innerHTML = 'Valitsit liian pienen numeron ja käytit kaikki arvauksesi,<br> oikea luku on ' + arvoLuku + '<br><button class="taas" onclick="pelaaTaas()">Pelaa taas</button>';
+      document.getElementById('' + arvaus+'').classList.add("vaarin");
+      document.getElementById(''+arvaus+'').disabled = true;
+    }
   }
 }
