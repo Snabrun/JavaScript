d8130d4638414d067e2a8f6ed2ded37a1451c48d
Snabrun <egor.shkaroburov@edu.keuda.fi>
2022-10-25 22:32:28 +0300

    Viikon3 2 osa on valmis

--- JavaScript/Perustehtäviä/Viikon3.teht/2 osa/tehtava.js
+++ JavaScript/Perustehtäviä/Viikon3.teht/2 osa/tehtava.js
@@ -1,11 +1,10 @@
-function tehtava1()
+function laskePisteet()
 {
-  var points = 0
-  var sana = document.getElementById('word').value;
-  for (var i = 0; i < sana.length; i++)
+  var pisteet = 0;
+  var annettuSana = document.getElementById('scsana').value;
+  for(var i = 0; i < annettuSana.length; i++)
   {
-    switch (sana[i])
-    {
+    switch (annettuSana[i]) {
       case 'a':
       case 'e':
       case 'i':
@@ -18,24 +17,24 @@ function tehtava1()
       case 'N':
       case 'S':
       case 'T':
-      points ++;
-      break;
+        pisteet++;
+        break;
       case 'o':
-      case 'ä':
+      case 'Ã¤':
       case 'k':
       case 'l':
       case 'O':
-      case 'Ä':
+      case 'Ã„':
       case 'K':
       case 'L':
-      points +=2;
-      break;
+        pisteet+=2;
+        break;
       case 'u':
       case 'm':
       case 'U':
       case 'M':
-      points +=3;
-      break;
+        pisteet+=3;
+        break;
       case 'y':
       case 'h':
       case 'j':
@@ -48,29 +47,104 @@ function tehtava1()
       case 'P':
       case 'R':
       case 'V':
-      points +=4;
-      break;
-      case 'ö':
+        pisteet+=4;
+        break;
+      case 'Ã¶':
       case 'd':
-      case 'Ö':
+      case 'Ã–':
       case 'D':
-      points +=7;
-      break;
+        pisteet+=7;
+        break;
       case 'b':
       case 'f':
       case 'g':
       case 'B':
       case 'F':
       case 'G':
-      points +=8;
-      break;
+        pisteet+=8;
+        break;
       case 'c':
       case 'C':
-      points += 10;
-      break;
+        pisteet+=10;
+        break;
       default:
-      points += 12;
+        pisteet+=12;
     }
-    document.getElementById('v1').innerHTML = "<p>Sanan " + sana + " pisteet ovat: " + points + "</p>";
+    document.getElementById('tscrabble').innerHTML = "<p>Sanan " + annettuSana + " pisteet ovat: " + pisteet + "</p>";
   }
+}
+function muunnaTaulukoksi()
+{
+  var jono = [];
+  jono = [[1, 2, 1, 24], [8, 11, 9, 4], [7, 0, 7, 27], [7, 4, 28, 14], [3, 10, 26, 7]];
+  var taulu = '<table border="1">';
+  for(var j = 0; j < jono.length; j++)
+  {
+    taulu += '<tr>';
+    for(var k = 0; k < jono[j].length; k++)
+    {
+      taulu += '<td>' + jono[j][k] + '</td>';
+    }
+    taulu += '</tr>';
+  }
+  taulu += '</table>';
+  document.getElementById('ttaulu').innerHTML = taulu;
+}
+function teeLomake()
+{
+  var lomake = '<p>Anna tilinro:<input type="text" id="tilinro" value=""></p>';
+  var lomake = lomake + '<p>Anna etunimi:<input type="text" id="enimi" value=""></p>';
+  var lomake = lomake + '<p>Anna sukunimi:<input type="text" id="snimi" value=""></p>';
+  var lomake = lomake + '<p>Anna saldo:<input type="text" id="saldo" value=""></p>';
+  var lomake = lomake + '<p>Haluatko jatkaa (k/e):<input type="text" id="lopetus" value=""></p>';
+  document.getElementById('lomake').innerHTML = lomake;
+  laskuri = 0;
+  lopetus = 'e';
+  do {
+
+    lopetus = document.getElementById('lopetus').value;
+  }while(lopetus = 'k');
+
+}
+function arvoKortit()
+{
+  var pakka = [''];
+  var maat = ['&#9828;', '&#9827;', '&#9826;', '&#9825;'];
+  var numerot = ['A','2','3','4','5','6','7','8','9','10','J','Q','K'];
+  laskuri = 0;
+  for(m = 0; m < maat.length; m++)
+  {
+    for(n = 0; n < numerot.length; n++)
+    {
+      if(m == maat.length-1 && n == numerot.length-1)
+      {
+        pakka[laskuri] = maat[m] + numerot[n];
+      }
+      else {
+        pakka[laskuri] = maat[m]+numerot[n];
+        laskuri++;
+      }
+
+    }
+  }
+  var kortit = [];
+  for(l = 0; l < 5; l++)
+  {
+      temp = pakka[Math.floor(Math.random()*52)];
+      kortit[l] = temp;
+  }
+  document.getElementById('pakka').innerHTML = '<p>'+kortit+'</p>';
+}
+function arvoLotto()
+{
+  var lnumerot = [];
+  var temp;
+  for(var lotto = 0; lotto < 7; lotto++)
+  {
+    temp = Math.floor(Math.random()*40)+1;
+    lnumerot[lotto] = temp;
+  }
+  lnumerot.sort();
+
+  document.getElementById('ltaulu').innerHTML = '<p>'+lnumerot+'</p>';
 }
---
+++ JavaScript/Projektit/Projekti2/koodi.js
@@ -0,0 +1,91 @@
+var rivi1 = document.getElementById('m1');
+var rivi2 = document.getElementById('m2');
+var rivi3 = document.getElementById('m3');
+
+var arvattava = Math.floor(Math.random()*10) + 1;
+var arvaustenLukumaara = 1;
+let arvatutNumerot = [];
+var voitot = 0;
+var tappiot = 0;
+
+
+
+function napit(arvo)
+{
+  if(arvaustenLukumaara >= 3)
+  {
+    arvatutNumerot.push(arvo.value);
+    for (var n = 1; n <= 10; n++)
+    {
+      document.getElementById(n).classList.add("disabled");
+      document.getElementById(n).disabled = true;
+    }
+    m2.innerHTML = "Arvausten lukumäärä: " + arvaustenLukumaara;
+    m3.innerHTML = "Arvatut numerot ovat: " + arvatutNumerot;
+  }
+  else
+  {
+    arvatutNumerot.push(arvo.value);
+  }
+  if(arvo.value < arvattava)
+  {
+    m1.innerHTML = "Annoit liian pienen numeron!";
+    for(var i = 1; i <= arvo.value; i++)
+    {
+      document.getElementById(i).classList.add("disabled");
+      document.getElementById(i).disabled = true;
+    }
+    if(arvaustenLukumaara >= 3)
+    {
+      tappiot++;
+    }
+  }
+  else if(arvo.value > arvattava)
+  {
+    m1.innerHTML = "Annoit liian suuren numeron!";
+    for(var j = arvo.value; j <= 10; j++)
+    {
+      document.getElementById(j).classList.add("disabled");
+      document.getElementById(j).disabled = true;
+    }
+    if(arvaustenLukumaara >= 3)
+    {
+      tappiot++;
+    }
+  }
+  else
+  {
+    m1.innerHTML = "Oikein, tämä oli koneen arpoma numero";
+    document.getElementById(arvo.value).classList.add("oikein");
+    document.getElementById(arvo.value).disabled = true;
+    voitot++;
+    for(var k = 1; k <= 10; k++)
+    {
+      if(k == arvo.value && arvo.value !=10)
+      {
+        k++;
+      }
+      document.getElementById(k).classList.add("disabled");
+      document.getElementById(k).disabled = true;
+    }
+  }
+  m2.innerHTML = "Arvausten lukumäärä: " + arvaustenLukumaara;
+  m3.innerHTML = "Arvatut numerot ovat: " + arvatutNumerot;
+  arvaustenLukumaara++;
+  document.getElementById('tilanne').innerHTML = "voitot: " + voitot + ' / tappiot: ' + tappiot;
+}
+function uusix()
+{
+  arvaustenLukumaara = 1;
+  arvatutNumerot.length = 0;
+  arvattava = Math.floor(Math.random()*10) + 1;
+  for(var m = 1; m <= 10; m++)
+  {
+    document.getElementById(m).classList.remove("disabled");
+    document.getElementById(m).classList.remove("oikein");
+    document.getElementById(m).disabled = false;
+    m1.innerHTML = "";
+    m2.innerHTML = "Arvausten lukumäärä: 0";
+    m3.innerHTML = "Arvatut numerot: -";
+  }
+}
