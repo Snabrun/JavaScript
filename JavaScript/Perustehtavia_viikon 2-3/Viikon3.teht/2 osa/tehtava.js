2105b5f9c4dc98def309427cf0c449269d956bea
Egor Shkaroburov <egor.shkaroburov@edu.keuda.fi>
2022-11-02 11:20:05 +0200

    värit fiksattu

--- JavaScript/Perustehtavia_viikon 2-3/Viikon3.teht/2 osa/tehtava.js
+++ JavaScript/Perustehtavia_viikon 2-3/Viikon3.teht/2 osa/tehtava.js
@@ -78,6 +78,7 @@ function muunnaTaulukoksi()
   var jono = [];
   jono = [[1, 2, 1, 24], [8, 11, 9, 4], [7, 0, 7, 27], [7, 4, 28, 14], [3, 10, 26, 7]];
   var taulu = '<table border="1">';
+  console.log(jono.length);
   for(var j = 0; j < jono.length; j++)
   {
     taulu += '<tr>';
