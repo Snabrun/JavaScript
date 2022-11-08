5547342e48e0b68e266b1f5c1232e7eee10d425e
Snabrun <egor.shkaroburov@edu.keuda.fi>
2022-11-08 17:08:56 +0200

    Projekti 5 JS aloitus

--- JavaScript/Projekti-5/peli.js
+++ JavaScript/Projekti-5/peli.js
@@ -1,9 +1,35 @@
-const cards = document.querySelectorAll(".muistipeli-kortti");
+const cards = document.querySelectorAll(".memory-card");
 
-const flipCard = e => {
-  console.log("EVENT ON CARD", e.target.parentElement);
-}
+let hasFlippedCard = false;
+let firstCard, secondCard;
+
+const flipCard = event => {
+  const target = event.target.parentElement;
+
+  taget.classList.add("flip");
+/*
+  if (!hasFlippedCard) {
+    hasFlippedCard = false;
+    secondCard = target;
+  }
+    else {
+      hasFlippedCard = false;
+      secondCard = target;
+      checkForMatch();
+    } */
+};
+/*
+const checkForMatch = () => {
+  if (firstCard.dataset.framework === secondCard.dataset.framework) {
+    firstCard.removeElementListener("click", flipCard);
+    secondCard.removeElementListener("click", flipCard);
+  } else {
+    firstCard.classList.remove('flip')
+  }
+};
+*/
+
 
 cards.forEach(card => {
-  card.addEventListener("click", flipCard)
+  card.addEventListener("click", flipCard);
 });
