3d80dc30f1dc562440b187d3a405a8caad299805
Snabrun <egor.shkaroburov@edu.keuda.fi>
2022-12-05 00:43:27 +0200

    Projekti-6 on valmis

--- JavaScript/Projekti-6/projekti-6.js
+++ JavaScript/Projekti-6/projekti-6.js
@@ -2,23 +2,59 @@ function formValue(form) {
   var firstName = form.firstName.value;
   var lastName = form.lastName.value;
   var address = form.address.value;
-  var postIndex = form.index.value;
+  var formIndex = form.index.value;
   var city = form.city.value;
   var phone = form.phone.value;
   var email = form.email.value;
   //tarkistetaan etunimesi
   console.log(firstName);
+  console.log(email);
   if (firstName.length < 3) {
-    alert("Anna nimi, jossa on vähintään kolme kirjainta")
+    alert("Anna nimesi, jossa on vähintään kolme kirjainta");
     return false;
   }
   //tarkistetaan sukunimesi
   if (lastName.length < 3) {
-    alert("Anna sukunimi, jossa on vähintään kolme kirjainta")
+    alert("Anna sukunimesi, jossa on vähintään kolme kirjainta");
     return false;
   }
-  if (address < 3) {
-
+  if (address.length < 3) {
+    alert("Anna osoitteesi, jossa on vähintään kolme kirjainta");
+    return false;
   }
+  if (formIndex.length < 5) {
+    alert("Anna indeksisi, jossa on vähintään viisi lukua");
+    return false;
+  }
+  if (city.length < 3) {
+    alert("Anna kaupunkisi, jossa on vähintään kolme kirjainta");
+    return false;
+  }
+  if (phone.length < 6) {
+    alert("Anna puhelimesi, jossa on vähintään kuusi lukua");
+    return false;
+  }
+
+
+  else {
+    console.log("test");
+    var firstName = document.getElementById("first-name").value;
+    var lastName = document.getElementById("last-name").value;
+    var address = document.getElementById("form-address").value;
+    var postIndex = document.getElementById("form-index").value;
+    var city = document.getElementById("form-city").value;
+    var phone = document.getElementById("form-phone").value;
+    var userMail = document.getElementById("form-email").value;
+    const userInfo = {
+      firstName,
+      lastName,
+      address,
+      postIndex,
+      city,
+      phone,
+    };
+    localStorage.setItem(userMail, JSON.stringify(userInfo));
+  }
+
 
 }
